d30f9fa8982b99e3ae641059064d6e6cd7666a5d
aiquantg <udai.solanki@gmail.com>
2023-05-10 17:30:04 +0530

    Update Homework1.hs

--- code/Week05/homework/Homework1.hs
+++ code/Week05/homework/Homework1.hs
@@ -4,18 +4,34 @@
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
-                                       mkMintingPolicyScript)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($))
+import           Plutus.V1.Ledger.Interval (contains)
+import           Plutus.V2.Ledger.Api (BuiltinData, POSIXTime, PubKeyHash, MintingPolicy,
+                                            ScriptContext (scriptContextTxInfo),
+                                            TxInfo (txInfoValidRange),
+                                            Validator, to, mkMintingPolicyScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)
+import           PlutusTx             (applyCode, compile, liftCode)
+import           PlutusTx.Prelude     (Bool, traceIfFalse, ($), (&&), (.))
 import           Utilities            (wrapPolicy)
+
+
 
 {-# INLINABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = False -- FIX ME!
+mkDeadlinePolicy _pkh _deadline () _ctx =  signedByBeneficiary  && deadlineReached
+
+    where
+        info :: TxInfo
+        info = scriptContextTxInfo _ctx
+        
+        signedByBeneficiary :: Bool
+        signedByBeneficiary = txSignedBy info $  _pkh
+
+        deadlineReached :: Bool
+        deadlineReached = contains (to $  _deadline) $ txInfoValidRange info
+
 
 {-# INLINABLE mkWrappedDeadlinePolicy #-}
 mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
@@ -24,5 +40,3 @@ mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy pkh deadlin
 deadlinePolicy :: PubKeyHash -> POSIXTime -> MintingPolicy
 deadlinePolicy pkh deadline = mkMintingPolicyScript $
     $$(PlutusTx.compile [|| mkWrappedDeadlinePolicy ||])
-        `PlutusTx.applyCode` PlutusTx.liftCode pkh
-        `PlutusTx.applyCode` PlutusTx.liftCode deadline

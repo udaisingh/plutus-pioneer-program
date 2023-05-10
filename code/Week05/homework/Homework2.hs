2b34de10bc77ba461330b3ff489e150652b01dcf
aiquantg <udai.solanki@gmail.com>
2023-05-10 17:31:09 +0530

    Update Homework2.hs

--- code/Week05/homework/Homework2.hs
+++ code/Week05/homework/Homework2.hs
@@ -6,7 +6,7 @@
 module Homework2 where
 
 import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy,
-                                       ScriptContext, TokenName, TxOutRef,
+                                       ScriptContext, TokenName, TxOutRef, TxInfo,
                                        mkMintingPolicyScript)
 import qualified PlutusTx
 import           PlutusTx.Prelude     (Bool (False), ($), (.))
@@ -15,8 +15,20 @@ import           Utilities            (wrapPolicy)
 {-# INLINABLE mkEmptyNFTPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
-mkEmptyNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkEmptyNFTPolicy _oref () _ctx = False -- FIX ME!
+mkPolicy :: TxOutRef -> () -> ScriptContext -> Bool
+mkPolicy oref () ctx = traceIfFalse "UTxO not consumed"   hasUTxO           &&
+                       traceIfFalse "wrong amount minted" checkMintedAmount
+  where
+    info :: TxInfo
+    info = scriptContextTxInfo ctx
+
+    hasUTxO :: Bool
+    hasUTxO = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info
+
+    checkMintedAmount :: Bool
+    checkMintedAmount = case flattenValue (txInfoMint info) of
+        [(cs, tn', amt)] -> cs  == ownCurrencySymbol ctx && tn' == tn && amt == 1
+        _                -> False
 
 {-# INLINABLE mkWrappedEmptyNFTPolicy #-}
 mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
@@ -24,3 +36,6 @@ mkWrappedEmptyNFTPolicy = wrapPolicy . mkEmptyNFTPolicy
 
 nftPolicy :: TxOutRef -> TokenName -> MintingPolicy
 nftPolicy oref tn = mkMintingPolicyScript $ $$(PlutusTx.compile [|| mkWrappedEmptyNFTPolicy ||]) `PlutusTx.applyCode` PlutusTx.liftCode oref
+
+        `PlutusTx.applyCode` PlutusTx.liftCode pkh
+        `PlutusTx.applyCode` PlutusTx.liftCode deadline

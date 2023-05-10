4264e414066207ae5978016133bf33577fea8be5
aiquantg <udai.solanki@gmail.com>
2023-05-10 17:11:45 +0530

    Update Homework1.hs

--- code/Week02/homework/Homework1.hs
+++ code/Week02/homework/Homework1.hs
@@ -7,7 +7,8 @@ module Homework1 where
 
 import qualified Plutus.V2.Ledger.Api as PlutusV2
 import           PlutusTx             (compile)
-import           PlutusTx.Prelude     (Bool (..), BuiltinData)
+import           PlutusTx.Prelude     (Bool (..), (&&), BuiltinData)
+--import           Prelude               (Bool(..), (&&))
 import           Utilities            (wrapValidator)
 
 ---------------------------------------------------------------------------------------------------
@@ -15,8 +16,12 @@ import           Utilities            (wrapValidator)
 
 {-# INLINABLE mkValidator #-}
 -- This should validate if and only if the two Booleans in the redeemer are True!
+
+
 mkValidator :: () -> (Bool, Bool) -> PlutusV2.ScriptContext -> Bool
-mkValidator _ _ _ = False
+-- use && operator
+-- bool = b1, bool = b2
+mkValidator _ (b1, b2) _ = b1 && b2
 
 wrappedVal :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 wrappedVal = wrapValidator mkValidator
